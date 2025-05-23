f617e5a374075251a1a5f0e498b946f52d4094cd
KrishnaSharma <krishnasharma246810@gmail.com>
2025-05-23 21:04:21 +0530

    styling changes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+
 let square = document.querySelector(".fa-square");
 let trash = document.querySelector(".fa-trash");
 let btn = document.querySelector("button");
 let container = document.querySelector(".container");
 let inp = document.querySelector("input");
 
-btn.addEventListener("click", function() {
+
+btn.addEventListener("click", function () {
+  
+  if (inp.value.trim() === "") {
+    alert("Enter valid task");
+    return;
+  }
   let el = document.createElement("p");
   el.innerText = inp.value;
 
@@ -23,15 +30,16 @@ btn.addEventListener("click", function() {
   container.appendChild(task);
   inp.value = "";
 
-});
+})
 
-container.addEventListener("click", function(e) {
+
+container.addEventListener("click", function (e) {
   if (e.target.classList.contains("fa-square")) {
     e.target.parentElement.classList.add("done");
   }
 });
 
-container.addEventListener("click", function(e) {
+container.addEventListener("click", function (e) {
   if (e.target.classList.contains("fa-trash")) {
     e.target.parentElement.remove();
   }
